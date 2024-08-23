646e054f0170c518a2a9f8e53ee25c27b5ee3a42
Samgibs <Captainsamuelqs@gmail.com>
2024-08-23 16:16:04 +0300

    fixed cors

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'http://167.99.157.253:8000',
 });
 
 axiosInstance.interceptors.request.use(
